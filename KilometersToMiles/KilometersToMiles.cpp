03ec22483438e476bba4d27b100401a161a0f651
GameBoyYeet <ericshurocks@gmail.com>
2021-07-22 15:59:36 -0700

    Finished KilometersToMiles program

--- KilometersToMiles/KilometersToMiles.cpp
+++ KilometersToMiles/KilometersToMiles.cpp
@@ -5,8 +5,13 @@
 using namespace std;
 int main()
 {
-    cout << "This program will convert miles to kilometers\nEnter an amount of miles.";
-
+    cout << "This program will convert miles to kilometers.\nEnter an amount of miles.\n";
+    double km;
+    double mi;
+    cin >> km;
+    mi = km * 1.609;
+    cout << km << " kilometeres to miles = " << mi << '\n';
+    system("pause");
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
