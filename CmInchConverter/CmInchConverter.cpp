bf2ff1b34572dc134ef0c4aaa7980f782cbf95f4
GameBoyYeet <ericshurocks@gmail.com>
2021-07-22 16:23:44 -0700

    Finished CmInchConverter (may be broken)

--- CmInchConverter/CmInchConverter.cpp
+++ CmInchConverter/CmInchConverter.cpp
@@ -1,17 +1,25 @@
-// KilometersToMiles.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// CmInchConverter.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 using namespace std;
 int main()
 {
-    cout << "This program will convert miles to kilometers.\nEnter an amount of miles.\n";
-    double km;
-    double mi;
-    cin >> km;
-    mi = km * 1.609;
-    cout << km << " kilometers to miles = " << mi << '\n';
-    system("pause");
+    constexpr double cm_per_inch = 2.54;
+    double length;
+    char unit;
+    cout << "This program can convert centimeters to inches and vice versa.\nEnter a length followed by a unit (c or i)\n";
+    cin >> length >> unit;
+    switch (unit)
+    {
+    case 'i':
+        cout << length << "in == " << length / cm_per_inch << "in\n";
+        break;
+    default:
+        cout << "Sorry, I don't know a unit called " << unit << ". Re-open the program to try again.\n";
+        system("pause");
+        break;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
