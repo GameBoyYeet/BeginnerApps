fafb555680f491343048ba597fb3c2071995c83d
GameBoyYeet <ericshurocks@gmail.com>
2021-07-23 13:09:37 -0700

    Make string-comparison in ComputeAvg case-insensitive

--- ComputeAvg/ComputeAvg.cpp
+++ ComputeAvg/ComputeAvg.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 vector<double> nums;
 string currentNumText;
 double currentNum;
+
+int case_insensitive_match(string s1, string s2) {
+	//convert s1 and s2 into lower case strings
+	transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+	transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+	if (s1.compare(s2) == 0)
+		return 1; //The strings are same
+	return 0; //not matched
+}
 
 int main()
 {
@@ -12,7 +22,7 @@ int main()
 	while (true)
 	{
 		cin >> currentNumText;
-		if (currentNumText != "done") {
+		if (not case_insensitive_match(currentNumText, "done")) {
 			currentNum = stod(currentNumText);
 			nums.push_back(currentNum);
 		}
