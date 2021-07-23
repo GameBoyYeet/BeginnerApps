89c0e12855f8a8a9b20329e4d68d51c1e6e4e766
GameBoyYeet <ericshurocks@gmail.com>
2021-07-22 17:56:11 -0700

    Added two "modes" to SquareListGenerator

--- SquareListGenerator/SquareListGenerator.cpp
+++ SquareListGenerator/SquareListGenerator.cpp
@@ -6,15 +6,73 @@
 using namespace std;
 int main()
 {
-    cout << "This program will generate a list of squares within the range that you choose.\nEnter the highest number that you want squared. (no decimals!)\n";
-    double high;
-    cin >> high;
-    for (int i = 0; i <= high; i++)
-    {
-        cout << i << '\t' << pow(i,2) << '\n';
-        i++;
+    cout << "This program will generate a list of squares or square roots within the range that you choose.\nWould you like to generate a list of squares (s) or square roots? (r)\n";
+    char mode;
+    cin >> mode;
+    double highestSquare; // highest square
+    double highestRoot; // highest sqrt
+    switch (mode) {
+    case 's':
+        cout << "Enter the highest number that you want squared. (no decimals!)\n";
+        cin >> highestSquare;
+        highestSquare++;
+        if (highestSquare >= 50000)
+        {
+            char confirm = ' ';
+            cout << "50,000+ squares will take a significant amount of time. Do you want to proceed? (y/n)\n";
+            cin >> confirm;
+            switch (confirm) {
+                case 'y':
+                    for (int i = 0; i <= highestSquare; i++)
+                    {
+                        cout << i << '\t' << pow(i, 2) << '\n';
+                        i++;
+                    }
+                default:
+                    system("pause");
+                    exit(0);
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= highestSquare; i++)
+            {
+                cout << i << '\t' << pow(i, 2) << '\n';
+                i++;
+            }
+        }
+        
+        break;
+    case 'r':
+        cout << "Enter the highest number that you want the square root of. (no decimals!)\n";
+        cin >> highestRoot;
+        highestRoot++;
+        if (highestRoot >= 50000) 
+        {
+            char confirm = ' ';
+            cout << "50,000+ square roots will take a significant amount of time. Do you want to proceed? (y/n)\n";
+            switch (confirm) 
+            {
+                case 'y':
+                    for (int i = 0; i <= highestRoot; i++)
+                    {
+                        cout << i << '\t' << sqrt(i) << '\n';
+                    }
+                    system("pause");
+                    exit(0);
+                    break;
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= highestRoot; i++)
+            {
+                cout << i << '\t' << sqrt(i) << '\n';
+            }
+            system("pause");
+            exit(0);
+        }
     }
-    system("pause");
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
