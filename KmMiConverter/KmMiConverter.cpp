cc5740ea22256c9dcc532c442bac98f6b3ca6d5d
GameBoyYeet <ericshurocks@gmail.com>
2021-07-23 19:08:05 -0700

    Replaced KilometersToMiles with KmMiConverter

---
+++ KmMiConverter/KmMiConverter.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	constexpr double km_in_mi = 1.609;
+	double num;
+	double result;
+	char unit;
+	cout << "This program can convert kilometers to miles and vice versa.\nEnter a number followed by a unit (k or m)\n";
+	cin >> num >> unit;
+	switch (unit)
+	{
+		case 'k':
+			result = num * 1.609;
+			cout << num << " kilometers is approx. " << result << " miles\n";
+			system("pause");
+			exit(0);
+			break;
+		case 'm':
+			result = num / 1.609;
+			cout << num << " miles is approx. " << result << "kilometers\n";
+			break;
+		default:
+			if (num == 0)
+			{
+				cout << "You didn't provide a number or your number was 0. Re-open the program to try again\n";
+				system("pause");
+				exit(0);
+			}
+			else
+			{
+				cout << "Your unit, " << unit << " is not valid. Re-open the program to try again.\n";
+				system("pause");
+				exit(0);
+			}
+			break;
+	}
+}
